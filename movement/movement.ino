357ad9f0e648ae98f1f2c5281142df90f281d792
Dhruv Batra <dhruvbatra2@gmail.com>
2023-03-26 08:11:12 -0400

    movement

--- movement/movement.ino
+++ movement/movement.ino
@@ -1,3 +1,5 @@
+//ARDUINO
+
 #define enA 9
 #define in1 4
 #define in2 5
@@ -5,6 +7,11 @@
 #define in3 6
 #define in4 7
 
+void Forward();
+void Stop();
+void Left();
+void Right();
+void custom_delay();
 
 void setup() {
   pinMode(enA, OUTPUT);
@@ -13,17 +20,70 @@ void setup() {
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(9600); // start serial communication with the Arduino Uno at 9600 baud
 }
 
 void loop() {
-    // Set Motor A backward
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    // Set Motor B backward
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-    analogWrite(enA, 255); // Send PWM signal to motor A
-    analogWrite(enB, 255); // Send PWM signal to motor B
-    Serial.println("Hello");
+  int i;
+  for(i=0; i<300;i++){
+    Forward();
+    delay(10);
+  }
+  for(i=0; i<300;i++){
+    Stop();
+    delay(10);
+  }
+  for(i=0; i<300;i++){
+    Right();
+    delay(10);
+  }
+  for(i=0; i<300;i++){
+    Left();
+    delay(10);
+  }
+}
+
+void Forward(){
+  Serial.println("Forward");
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  analogWrite(enA, 255); // Send PWM signal to motor A
+  analogWrite(enB, 255); // Send PWM signal to motor B
+  custom_delay();
+}
+void Stop(){
+  Serial.println("Stop");
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  analogWrite(enA, 255); // Send PWM signal to motor A
+  analogWrite(enB, 255); // Send PWM signal to motor B
+  custom_delay();
+}
+void Right(){
+  Serial.println("Right");
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  analogWrite(enA, 255); // Send PWM signal to motor A
+  analogWrite(enB, 255); // Send PWM signal to motor B
+  custom_delay();
+}
+void Left(){
+  Serial.println("Left");
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  analogWrite(enA, 255); // Send PWM signal to motor A
+  analogWrite(enB, 255); // Send PWM signal to motor B
+  custom_delay();
+}
+
+void custom_delay(){
+  delay(20);
 }
